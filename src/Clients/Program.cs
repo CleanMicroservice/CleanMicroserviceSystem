5e5806a7b40e327e57cf5e12e818308425b45038
Leon <Leon.Engineer@hotmail.com>
2022-07-31 16:45:36 +0800

    Rename folder;

--- src/Clients/Program.cs
+++ src/Clients/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TemplateWebApp.Client;
+using TemplateWebApp.WebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
