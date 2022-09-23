bb9c52335db70770dd7da040693f15f66b37a789
Leon <Leon.Engineer@hotmail.com>
2022-09-24 01:28:56 +0800

    Install AntDesign for Balzor package;

--- src/CleanMicroserviceSystem.WebApp/Program.cs
+++ src/CleanMicroserviceSystem.WebApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddAntDesign();
 builder.Services.AddHttpClient("CleanMicroserviceSystem.WebApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
