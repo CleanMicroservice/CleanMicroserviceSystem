e15d49c3f36f996f656ca036c1a0a87f020e1796
Leon <Leon.Engineer@hotmail.com>
2022-09-25 00:38:12 +0800

    Implement domain's contracts and entities;

---
+++ src/CleanMicroserviceSystem.Service.Domain.Abstraction/Contracts/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanMicroserviceSystem.Service.Domain.Abstraction.Contracts;
+
+public interface IEntity<TID> : IEntity
+{
+    public TID ID { get; set; }
+}
+
+public interface IEntity
+{
+}
---
+++ src/CleanMicroserviceSystem.Service.Domain.Abstraction/Entities/GenericOption.cs
@@ -0,0 +1,14 @@
+﻿using CleanMicroserviceSystem.Service.Domain.Abstraction.Contracts;
+
+namespace CleanMicroserviceSystem.Service.Domain.Abstraction.Entities;
+
+public class GenericOption : AuditableEntity<int>
+{
+    public string OptionName { get; set; }
+
+    public string? OptionValue { get; set; }
+
+    public string? Category { get; set; }
+
+    public string? OwnerLevel { get; set; }
+}
