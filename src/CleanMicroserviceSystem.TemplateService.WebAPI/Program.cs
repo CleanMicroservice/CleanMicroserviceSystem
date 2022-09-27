4acbb8d68294162f38f277c5d79e34fe208d96e7
Leon <Leon.Engineer@hotmail.com>
2022-09-27 23:32:56 +0800

    AddHttpContextAccessor(); Register application and domain events;

--- src/CleanMicroserviceSystem.TemplateService.WebAPI/Program.cs
+++ src/CleanMicroserviceSystem.TemplateService.WebAPI/Program.cs
@@ -8,15 +8,22 @@ logger.Info($"{assemblyName.Name} launches, TargetFrameworkName={setupInformatio
 
 try
 {
+    AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+    Console.CancelKeyPress += Console_CancelKeyPress;
+
     var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
+    builder.Services.AddHttpContextAccessor();
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
     var app = builder.Build();
+    var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+    lifetime.ApplicationStarted.Register(ApplicationLifetime_ApplicationStarted);
+    lifetime.ApplicationStopping.Register(ApplicationLifetime_ApplicationStopping);
+    lifetime.ApplicationStopped.Register(ApplicationLifetime_ApplicationStopped);
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
@@ -42,3 +49,21 @@ finally
     logger.Info($"{assemblyName.Name} shutdown, Version={assemblyName.Version}");
     LogManager.Shutdown();
 }
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+    => logger.Info($"{nameof(Console_CancelKeyPress)} => {(e.Cancel ? "Cancel" : "Not Cancel")}, {e.SpecialKey.ToString()}");
+
+void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+    => logger.Info($"{nameof(CurrentDomain_ProcessExit)}");
+
+void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    => logger.Error($"{nameof(CurrentDomain_UnhandledException)} => {(e.IsTerminating ? "Terminating" : "Not Terminating")}, {e.ExceptionObject.ToString()}");
+
+void ApplicationLifetime_ApplicationStarted()
+    => logger.Info($"{nameof(ApplicationLifetime_ApplicationStarted)}");
+
+void ApplicationLifetime_ApplicationStopping()
+    => logger.Info($"{nameof(ApplicationLifetime_ApplicationStopping)}");
+
+void ApplicationLifetime_ApplicationStopped()
+    => logger.Info($"{nameof(ApplicationLifetime_ApplicationStopped)}");
