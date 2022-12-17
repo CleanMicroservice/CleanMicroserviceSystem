8dcf39aca69e6b13ba35217c87e94b4cc6a579c3
Leon <Leon.Engineer@hotmail.com>
2022-12-18 01:45:06 +0800

    Move Common projects;

---
+++ src/CommonLibrary/Authentication/CleanMicroserviceSystem.Authentication/Extensions/CleanMicroserviceSystemAuthenticationExtension.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using CleanMicroserviceSystem.Authentication.Configurations;
+using CleanMicroserviceSystem.Authentication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CleanMicroserviceSystem.Authentication.Extensions;
+public static class CleanMicroserviceSystemAuthenticationExtension
+{
+    public static IServiceCollection AddJwtBearerAuthentication(
+        this IServiceCollection services,
+        JwtBearerConfiguration configuration)
+    {
+        services
+            .Configure(new Action<JwtBearerConfiguration>(options =>
+            {
+                options.JwtAudience = configuration.JwtAudience;
+                options.JwtExpiryInMinutes = configuration.JwtExpiryInMinutes;
+                options.JwtIssuer = configuration.JwtIssuer;
+                options.JwtSecurityKey = configuration.JwtSecurityKey;
+            }))
+            .AddScoped<IJwtBearerTokenGenerator, JwtBearerTokenGenerator>()
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Events = new JwtBearerEvents()
+                {
+                    OnForbidden = context => Task.CompletedTask,
+                    OnAuthenticationFailed = context => Task.CompletedTask,
+                };
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    // Correction of expiration time's offset
+                    ClockSkew = TimeSpan.Zero,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration.JwtIssuer,
+                    ValidAudience = configuration.JwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.JwtSecurityKey))
+                };
+                options.Validate();
+            });
+
+        return services;
+    }
+}
