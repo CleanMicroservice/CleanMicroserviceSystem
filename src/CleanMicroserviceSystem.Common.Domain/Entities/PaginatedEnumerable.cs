4d9d9c86667f2b53693843829e59df5c8fec0f62
Leon <Leon.Engineer@hotmail.com>
2022-12-17 13:16:02 +0800

    Paginated enumerable;

---
+++ src/CleanMicroserviceSystem.Common.Domain/Entities/PaginatedEnumerable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace CleanMicroserviceSystem.Common.Domain.Entities;
+
+public class PaginatedEnumerable<T> : IEnumerable<T>
+{
+    private readonly IEnumerable<T> values;
+    public readonly int StartOfPage;
+    public readonly int CountPerPage;
+    public readonly int OriginCount;
+    public readonly int CurrentPageIndex;
+    public readonly int TotalPageCounts;
+
+    public PaginatedEnumerable(
+        IEnumerable<T> values,
+        int startOfPage,
+        int countPerPage,
+        int originCount)
+    {
+        this.values = values;
+        this.StartOfPage = startOfPage;
+        this.CountPerPage = countPerPage;
+        this.OriginCount = originCount;
+
+        this.CurrentPageIndex = this.StartOfPage / this.CountPerPage;
+        this.TotalPageCounts = (int)Math.Ceiling((double)OriginCount / this.CountPerPage);
+    }
+
+    public IEnumerator<T> GetEnumerator() => values.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
