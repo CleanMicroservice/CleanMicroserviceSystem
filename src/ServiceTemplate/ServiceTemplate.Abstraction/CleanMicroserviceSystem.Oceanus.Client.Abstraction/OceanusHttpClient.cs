f0995f74889d69ca83591a2b80f6ddbc4fe3cc3b
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-10 12:01:16 +0800

    Upgrade packages;

---
+++ src/ServiceTemplate/ServiceTemplate.Abstraction/CleanMicroserviceSystem.Oceanus.Client.Abstraction/OceanusHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMicroserviceSystem.Oceanus.Client.Abstraction
+{
+    public class OceanusHttpClient : HttpClient
+    {
+    }
+}
