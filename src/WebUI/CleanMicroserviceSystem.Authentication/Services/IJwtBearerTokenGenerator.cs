768ef7e119fe171884f2427790ef6b37dc4c29dc
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-18 19:19:39 +0800

    Remove useless project;

--- src/WebUI/CleanMicroserviceSystem.Authentication/Services/IJwtBearerTokenGenerator.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Security.Claims;
-
-namespace CleanMicroserviceSystem.Authentication.Services;
-public interface IJwtBearerTokenGenerator
-{
-    string GenerateSecurityToken(IEnumerable<Claim> claims);
-}
