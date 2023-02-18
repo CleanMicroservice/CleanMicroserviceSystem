2b07ee59a9f9d8b36678987c4ff7c9232b3d4d5f
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-18 19:12:55 +0800

    Install MASA.Blazor lib;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite/Program.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMasaBlazor();
 builder.Services.AddHttpClient("CleanMicroserviceSystem.Aphrodite.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
