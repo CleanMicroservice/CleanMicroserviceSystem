4345db5bf83bcc82fee7850511588c7192637c08
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-18 18:57:19 +0800

    Update nuget packages;

--- src/CommonLibrary/Intermediary/CleanMicroserviceSystem.Intermediary/Extensions/CleanMicroserviceSystemIntermediaryExtension.cs
+++ src/CommonLibrary/Intermediary/CleanMicroserviceSystem.Intermediary/Extensions/CleanMicroserviceSystemIntermediaryExtension.cs
@@ -4,7 +4,10 @@ public static class CleanMicroserviceSystemIntermediaryExtension
 {
     public static IServiceCollection AddCleanMicroserviceSystemIntermediary(this IServiceCollection services)
         => services
-            .AddMediatR(typeof(CleanMicroserviceSystemIntermediaryExtension).Assembly)
+            .AddMediatR(options =>
+            {
+                options.RegisterServicesFromAssemblies(typeof(CleanMicroserviceSystemIntermediaryExtension).Assembly);
+            })
             .AddTransient(typeof(IPipelineBehavior<,>), typeof(IntermediaryPipelineBehavior<,>))
             .AddTransient<IIntermediaryPublisher, IntermediaryPublisher>();
 }
