821417231c5b33cc08464a67c2ab30baaf4f2b13
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-22 13:42:52 +0800

    Implement MainNavigationBar and PermissionNavigation components;

---
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Shared/NavigationItemBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CleanMicroserviceSystem.Aphrodite.Shared
+{
+    public partial class NavigationItemBase : ComponentBase
+    {
+        protected readonly NavigationManager navigationManager;
+
+        public NavigationItemBase(
+            NavigationManager navigationManager) : base()
+        {
+            this.navigationManager = navigationManager;
+        }
+    }
+}
