4e357df851e128bd53bbd49abbcbb2add0488882
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-24 14:53:41 +0800

    @inherits AphroditePageBase

---
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Pages/SystemPages/AphroditePageBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CleanMicroserviceSystem.Aphrodite.Pages.SystemPages
+{
+    public class AphroditePageBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IHttpClientFactory HttpClientFactory { get; set; }
+    }
+}
