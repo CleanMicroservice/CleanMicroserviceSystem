a26128b21e7498da65362aa4e34cd0fb7468b5bc
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-24 14:20:08 +0800

    Add profile and settings pages; Adjust router uri;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
@@ -3,6 +3,8 @@
     public class RouterContract
     {
         public const string DefaultUri = "/";
-        public const string LoginUri = "/systempages/login";
+        public const string LoginUri = "/account/login";
+        public const string ProfileUri = "/account/profile";
+        public const string SettingsUri = "/system/settings";
     }
 }
