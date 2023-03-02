a3cdd8e9d44a7c7c29e27647734b6cb3c99896a0
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-02 16:54:59 +0800

    Add Users/Roles/Clients pages and navigator;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
@@ -9,4 +9,7 @@ public class RouterContract
     public const string LoginUri = "/account/login";
     public const string ProfileUri = "/account/profile";
     public const string SettingsUri = "/system/settings";
+    public const string UsersUri = "/permission/users";
+    public const string RolesUri = "/permission/roles";
+    public const string ClientsUri = "/permission/clients";
 }
