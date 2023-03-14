2565afd6c21a78bf4756327794211c0db08cc473
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-14 16:00:08 +0800

    Fix Ocelot pipeline issue;

--- src/MicroserviceComponents/Gateway/CleanMicroserviceSystem.Uranus.WebAPI/UranusProgram.cs
+++ src/MicroserviceComponents/Gateway/CleanMicroserviceSystem.Uranus.WebAPI/UranusProgram.cs
@@ -1,5 +1,4 @@
-﻿using CleanMicroserviceSystem.Gateway.Contract;
-using CleanMicroserviceSystem.Oceanus.Application.Abstraction.Configurations;
+﻿using CleanMicroserviceSystem.Oceanus.Application.Abstraction.Configurations;
 using CleanMicroserviceSystem.Oceanus.WebAPI.Abstraction;
 using CleanMicroserviceSystem.Uranus.Infrastructure;
 using Ocelot.Middleware;
@@ -10,15 +9,10 @@ public class UranusProgram : OceanusProgram
 {
     public UranusProgram(NLog.ILogger logger) : base(logger) { }
 
-    public override void ConfigurePipelines()
+    public override void ConfigurePipelinesBeforeAuth()
     {
-        // TODO: Ocelot
-        // Ocelot pipeline has no logging;
-        // WebAPILogMiddleware cause 415 error;
-        this.webApp.UseWhen(
-            context => context.Request.Path.StartsWithSegments(GatewayContract.GatewayUriPrefix),
-            builder => builder.UseOcelot().Wait());
-        base.ConfigurePipelines();
+        base.ConfigurePipelinesBeforeAuth();
+        this.webApp.UseOcelot().Wait();
     }
 
     public override void ConfigureServices()
