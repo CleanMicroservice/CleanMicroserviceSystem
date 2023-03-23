10a7f46f2d8285b3f6cb66e05d5f92ded3e757f2
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-23 18:42:23 +0800

    Enhance users page by MPagination;

---
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite.Application/Extensions/BoolExtension.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanMicroserviceSystem.Aphrodite.Application.Extensions;
+
+public static class BoolExtension
+{
+    public static string ToStatus(this bool enabled) => enabled ? "Enabled" : "Disabled";
+}
