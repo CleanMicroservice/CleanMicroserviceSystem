5f7b0a389cd0ac4d436a0c20b3ba23bbe2d66fc2
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-28 17:04:36 +0800

    Implement NavigatorItem, NavigatorGroup, NavigatorSubGroup;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite/Shared/NavigatorBase.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Shared/NavigatorBase.cs
@@ -4,7 +4,4 @@ namespace CleanMicroserviceSystem.Aphrodite.Shared;
 
 public partial class NavigatorBase : ComponentBase
 {
-    public static string ItemActiveClass = "fill-lighten-1";
-    public static string IconInactiveClass = "neutral-lighten-2";
-    public static string IconActiveClass = "primary";
 }
