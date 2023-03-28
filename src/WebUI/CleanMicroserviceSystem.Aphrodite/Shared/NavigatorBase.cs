b4143cad06a9cae49a78ddd9a44ee49c5b74e887
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-28 16:08:00 +0800

    Tiny;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite/Shared/NavigatorBase.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Shared/NavigatorBase.cs
@@ -4,5 +4,7 @@ namespace CleanMicroserviceSystem.Aphrodite.Shared;
 
 public partial class NavigatorBase : ComponentBase
 {
-    public static string ActiveClass = "fill-lighten-1";
+    public static string ItemActiveClass = "fill-lighten-1";
+    public static string IconInactiveClass = "neutral-lighten-2";
+    public static string IconActiveClass = "primary";
 }
