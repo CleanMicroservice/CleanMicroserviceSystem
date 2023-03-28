b93f4b0b33ec6162b7105da4b5495ef0e90e4833
Leon Liu <Leon.Engineer@hotmail.com>
2023-03-28 17:37:25 +0800

    Modify;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite/Pages/SystemPages/AphroditePageBase.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite/Pages/SystemPages/AphroditePageBase.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Masa.Blazor.Presets;
+using Microsoft.AspNetCore.Components;
 
 namespace CleanMicroserviceSystem.Aphrodite.Pages.SystemPages;
 
 public class AphroditePageBase : ComponentBase
 {
+    [CascadingParameter]
+    public IPageTabsProvider PageTabsProvider { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
 }
