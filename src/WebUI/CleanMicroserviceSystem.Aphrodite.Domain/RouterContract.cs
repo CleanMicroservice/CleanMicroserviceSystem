0f1c5ccc73ba8399b0380de5047d00985c730d3e
Leon <Leon.Engineer@hotmail.com>
2023-06-23 15:45:08 +0800

    Add K8S console pages;

--- src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
+++ src/WebUI/CleanMicroserviceSystem.Aphrodite.Domain/RouterContract.cs
@@ -23,4 +23,6 @@ public class RouterContract
     public const string PackagesUri = "/nugetserver/packages";
 
     public const string Level2VisualizationUri = "/level2visualization";
+
+    public const string K8SConsoleUri = "/k8sconsole";
 }
