6bc7b39690f4bbddcadeeb8c260c0e9aeae7ede5
Leon <Leon.Engineer@hotmail.com>
2023-07-26 13:37:13 +0800

    NTLMWeb supports return url;

--- src/MicroserviceComponents/IdentityService/CleanMicroserviceSystem.Themis.NTLMWeb/Pages/Index.cshtml.cs
+++ src/MicroserviceComponents/IdentityService/CleanMicroserviceSystem.Themis.NTLMWeb/Pages/Index.cshtml.cs
@@ -1,16 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CleanMicroserviceSystem.Themis.NTLMWeb.Pages;
+
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public string? ReturnUrl { get; protected set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
-    public void OnGet()
+    public void OnGet([FromQuery] string returnUrl)
     {
+        this.ReturnUrl = returnUrl;
     }
 }
