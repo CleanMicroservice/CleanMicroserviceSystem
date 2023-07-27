104046bb3d4db24429616c828fc9cec139aa574b
Leon <Leon.Engineer@hotmail.com>
2023-07-27 12:28:49 +0800

    Overrride ServerCertificateCustomValidationCallback to skip;

--- src/CommonLibrary/Authentication/CleanMicroserviceSystem.Authentication.Application/DefaultAuthenticationDelegatingHandler.cs
+++ src/CommonLibrary/Authentication/CleanMicroserviceSystem.Authentication.Application/DefaultAuthenticationDelegatingHandler.cs
@@ -1,14 +1,28 @@
 ﻿using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using CleanMicroserviceSystem.Authentication.Domain;
+using Microsoft.Extensions.Http.Logging;
 
 namespace CleanMicroserviceSystem.Authentication.Application;
 
 public class DefaultAuthenticationDelegatingHandler : DelegatingHandler
 {
     private readonly IAuthenticationTokenStore tokenStore;
+    private static readonly Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> serverCertificateCustomValidationCallback = (_, _, _, _) => true;
+    private bool handledUpdated = false;
+
+    public DefaultAuthenticationDelegatingHandler(
+        HttpMessageHandler handler,
+        IAuthenticationTokenStore tokenStore)
+        : base(handler)
+    {
+        this.tokenStore = tokenStore;
+    }
 
     public DefaultAuthenticationDelegatingHandler(
         IAuthenticationTokenStore tokenStore)
+        : base()
     {
         this.tokenStore = tokenStore;
     }
@@ -17,6 +31,7 @@ public class DefaultAuthenticationDelegatingHandler : DelegatingHandler
         HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
+        this.SetupServerCertificateCustomValidationCallback();
         this.AttachAuthorizationHeader(request).ConfigureAwait(false);
         return base.Send(request, cancellationToken);
     }
@@ -25,8 +40,34 @@ public class DefaultAuthenticationDelegatingHandler : DelegatingHandler
         HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
+        this.SetupServerCertificateCustomValidationCallback();
         await this.AttachAuthorizationHeader(request);
         return await base.SendAsync(request, cancellationToken);
+    }
+
+    protected void SetupServerCertificateCustomValidationCallback()
+    {
+        if (this.handledUpdated) return;
+        lock (this)
+        {
+            if (this.handledUpdated) return;
+            this.handledUpdated = true;
+            try
+            {
+                var innerHandler = this.InnerHandler;
+                if (innerHandler is LoggingHttpMessageHandler loggingHandler)
+                {
+                    innerHandler = loggingHandler.InnerHandler;
+                }
+                if (innerHandler is HttpClientHandler handler)
+                {
+                    handler.ServerCertificateCustomValidationCallback = serverCertificateCustomValidationCallback;
+                }
+            }
+            catch
+            {
+            }
+        }
     }
 
     protected async Task AttachAuthorizationHeader(HttpRequestMessage request)
